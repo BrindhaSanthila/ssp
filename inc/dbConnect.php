1ed09580b8482d5045de258867280b8231088556
santhiladatabot <santhiladatabot@gmail.com>
2023-04-11 10:13:28 +0530

    db connection after svn

--- inc/dbConnect.php
+++ inc/dbConnect.php
@@ -1,7 +1,7 @@
 <?php
 	
-	$database_username = 'root';
-	$database_password = '';
+	$database_username = 'santhila';
+	$database_password = 'santhila';
 	$pdo_conn = new PDO( 'mysql:host=192.168.1.42;dbname=ssp', $database_username, $database_password );
 	// $image_path = 'https://royalfurn.in/royal_furniture_mobapp/img/';
 	$image_path_back='../upload/'
